2f99ed3c7c8fe215650ec5146744822bba07dca9
Romeo Girard <romeo.girard@outlook.com>
2020-11-13 10:00:02 -0500

    Fin du cours de Vendredi

--- src/js/script.js
+++ src/js/script.js
@@ -9,7 +9,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
             slogan: "Eat. Pizza. Repeat.",
             type: ["pepperoni", "All Dress", "Baaaaaacon"],
             image: "images/pizza/main.jpg",
-            description: "NY style pesto ricotta onions chicken and bacon chicken wing peppers pie lasagna steak. Parmesan hawaiian extra sauce, onions pizza roll meatball stuffed burnt mouth black olives meatball thin crust bbq rib. Chicken and bacon pizza roll ham thin crust",    
+            description: "NY style pesto ricotta onions chicken and bacon chicken wing peppers pie lasagna steak. Parmesan hawaiian extra sauce, onions pizza roll meatball stuffed burnt mouth black olives meatball thin crust bbq rib. Chicken and bacon pizza roll ham thin crust",
             gallery: ["images/pizza/1.jpg", "images/pizza/2.jpg", "images/pizza/3.jpg"]
 
         }, {
@@ -18,7 +18,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
             slogan: "Bacon c'est comme un High-five pour ta bouche",
             type: ["À l'érable", "Bacon de sanglier", "avec du bacon", "au bacon", "avec du bacon"],
             image: "images/bacon/main.jpg",
-            description: "Leberkas hamburger ribeye, jerky frankfurter prosciutto tail fatback. Ham hock venison andouille ham turducken porchetta tenderloin t-bone flank beef ribs salami ribeye pork belly short ribs shankle. Cow capicola ground round, pancetta flank spare ribs swine pork chop chuck rump brisket sirloin",    
+            description: "Leberkas hamburger ribeye, jerky frankfurter prosciutto tail fatback. Ham hock venison andouille ham turducken porchetta tenderloin t-bone flank beef ribs salami ribeye pork belly short ribs shankle. Cow capicola ground round, pancetta flank spare ribs swine pork chop chuck rump brisket sirloin",
             gallery: ["images/bacon/1.jpg", "images/bacon/2.jpg", "images/bacon/3.jpg"]
         }, {
             id: "beigne",
@@ -26,10 +26,42 @@ document.addEventListener("DOMContentLoaded", function(event) {
             slogan: "Donut worry be happy",
             type: ["Glacé au chocolat", "Crème Boston", "Fraises", "Rousettes au miel", "À 'ancienne"],
             image: "images/donuts/main.jpg",
-            description: "Cake sweet tiramisu lollipop donut sprinkles dolor tiramisu marzipan jelly. Tiramisu muffin cookie orange dolor marzipan croissant sweet ipsum muffin. Tiramisu donut marzipan croissant lollipop orange cookie sugar vanilla cake. Sugar orange muffin sweet cake croissant sprinkles dolor jelly cake",    
+            description: "Cake sweet tiramisu lollipop donut sprinkles dolor tiramisu marzipan jelly. Tiramisu muffin cookie orange dolor marzipan croissant sweet ipsum muffin. Tiramisu donut marzipan croissant lollipop orange cookie sugar vanilla cake. Sugar orange muffin sweet cake croissant sprinkles dolor jelly cake",
             gallery: ["images/donuts/1.jpg", "images/donuts/2.jpg", "images/donuts/3.jpg"]
         }
     ]
 
+    var type = new URLSearchParams(window.location.search).get('type');
+    console.log(type);
+
+    var infoPage = informations.find(x => x.id === type);
+    console.log(infoPage);
+
+    var title = document.querySelector('.main-title');
+    title.innerHTML = infoPage.title;
+    console.log(title);
+
+    var slogan = document.querySelector('.sub-title');
+    slogan.innerHTML = infoPage.slogan;
+    console.log(slogan);
+
+    var desc = document.querySelector('.sub-description p');
+    desc.innerHTML = infoPage.description;
+    console.log(desc);
+
+    var image = document.querySelector('.sub-image img');
+    image.src = infoPage.image;
+    console.log(image);
+
+    var list = document.querySelector('.type-list');
+    list.innerHTML = "";
+    console.log(list);
+
+    for(var i = 0; i< infoPage.type.length; i++){
+        var li = document.createElement('li');
+        li.innerHTML = infoPage.type[i];
+
+        list.appendChild(li);
+    }
 
 });
